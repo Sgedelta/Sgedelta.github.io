8c1a2ef8266e6e94f5d4d598dc96a7109db60137
Sgedelta <Sgeaston04@gmail.com>
2026-01-08 01:22:28 -0500

    fixed await and tuned effects

--- js/whatAmI.js
+++ js/whatAmI.js
@@ -1,20 +1,25 @@
 const displayText = document.querySelector("#whatAmI");
+const cursorDisplay = document.querySelector("#whatAmICursor");
 const options = [
     "A Developer",
     "A Designer",
     "A Producer",
     "An Artist"
 ];
-const typingDelay = 50;
-const typingHold = 5000;
-const deleteDelay = 30;
+const typingDelay = 60;
+const typingHold = 4500;
+const deleteDelay = 50;
+const blinkSpeed = 700;
+let typing = false;
+let cursorState = false;
+
 
 //wait for given ms
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-const mainDisplayLoop = (repetition) => {
+const mainDisplayLoop = async (repetition) => {
 
     let rep = repetition % options.length;
 
@@ -22,23 +27,50 @@ const mainDisplayLoop = (repetition) => {
 
     //do typing effect
     displayText.textContent = "";
+    typing = true;
+    showOrHideCursor();
 
-    for(let i = 0; i < iAmStr.length; ++i) {
+    for(let i = 0; i <= iAmStr.length; ++i) {
         displayText.textContent = iAmStr.substring(0, i);
-        sleep(typingDelay);
+        await sleep(typingDelay * (1 + Math.random() * .4));
     }
     
-    sleep(typingHold);
+    typing = false;
+    showOrHideCursor();
+    await sleep(typingHold);
+    typing = true;
+    showOrHideCursor();
 
     //do deleting effect
     for(let i = iAmStr.length-1; i >= 0; --i) {
         displayText.textContent = iAmStr.substring(0, i);
-        sleep(deleteDelay);
+        await sleep(deleteDelay * (1 + Math.random() * .4));
     }
+
+    typing = false;
+    showOrHideCursor();
+    await sleep(typingHold * .3);
 
     //loop next option
     mainDisplayLoop(rep + 1);
 }
 
+const blinkCursor = async () => {
 
-mainDisplayLoop(1);
+    while(true) {
+        showOrHideCursor();
+
+        await sleep(blinkSpeed);
+
+        cursorState = !cursorState;
+    }
+}
+
+const showOrHideCursor = (show) => {
+    cursorDisplay.textContent = (cursorState || typing) ? "|" : "";
+}
+
+
+
+mainDisplayLoop(0);
+blinkCursor();
